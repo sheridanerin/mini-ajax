fe4c3d8b81a4c23041250919d9bd9a4d0674f854
Erin Sheridan <SheridanErin@gmail.com>
2015-10-21 14:01:46 -0600

    completed Ajax project

--- app.js
+++ app.js
@@ -1,62 +1,61 @@
 $(document).ready(function() {
 
-	$('body').on('click', '.js-get-users', function() {
-		return $.ajax({
-			method: 'GET',
-			url: 'http://reqr.es/api/users?page=1',
-		}).then(handleData, handleError)
-	});
+  	$('body').on('click', '.js-get-users', function () {
+    	return $.ajax({
+      		method: 'GET',
+      		url: 'http://reqr.es/api/users?page=1',
+    	}).then(function (res) {
+      		console.log(res);
+      		insertData(res.data);
+    	}, function (err) {
+      		console.error(err);
+    	});
+  	})
 
-	$('body').on('click', '.js-get-users', function() {
-		function handleSuccess(res) {
-			console.log(res);
-		}
+  	var insertData = function (arr) {
+    	var tpl = '<div>' +
+        	'User Info: <ul>' +
+        	'<li>First name: <span class="js-first">none</span></li>' +
+        	'<li>Last name: <span class="js-last">none</span></li>' +
+        	'</ul>' +
+        	'<hr>' +
+        	'</div>';
 
-		$.ajax({
-			method: 'GET',
-			url: 'http://reqr.es/api/users?pages=1',
-		}).then(handleSuccess);
-	});	
+    	arr.forEach(function (item, i) {
+      		var $copy = $(tpl);
 
-	var insertData = function (arr) {
-		var tpl = '<div>' +
-		'User Info: <ul>' +
-		'<li>First name: <span class="js-first">none</span></li>' +
-		'<li>Last name: <span class="js-last">none</span></li>' +
-		'</ul>' +
-		'<hr>' +
-		'</div';
+      		$copy.find('.js-first').text(item.first_name);
+      		$copy.find('.js-last').text(item.last_name);
 
-		arr.forEach(function (item, i) {
-			var $copy = $(tpl);
+      	$('.js-user-info-' + (i + 1)).html($copy);
+    	});
+ 	}
 
-			$copy.find('.js-first').text(item.first_name);
-			$copy.find('.js-last').text(item.last_name);
+  	$('body').on('click', '.js-add-user', function () {
+    	var userName = $('.js-name').val();
+    	var userJob = $('.js-job').val();
+    	return $.ajax({
+      		method: 'POST',
+      		url: 'http://reqr.es/api/users',
+      		data: {name: userName, job: userJob}
+    	}).then(function (res) {
+      		var tpl = '<li>name: <span class="js-name">none</span></li>' +
+       			'<li>job: <span class="js-job">none</span></li>' +
+        		'<li>id: <span class="js-id">none</span></li>' +
+        		'<li>created at:  <span class="js-created-at">none</span></li>';
 
-			$('.js-user-info' + (i + 1)).html($copy);
-		});
-	}
+            $copy = $(tpl);
+      		$copy.find('.js-name').text(res.name);
+      		$copy.find('.js-job').text(res.job);
+      		$copy.find('.js-id').text(res.id);
+      		$copy.find('.js-created-at').text(res.createdAt);
 
-	$('body').on('click', '.js-get-users', function() {
-		return $.ajax({
-			method: 'GET',
-			url: 'http://reqr.es/api/users?page=1',
-		}).then(function (res) {
-			consol.log(res);
-			insertData(res.data);
-		}, function (err) {
-			console.error(err);
-		});
-	})
-
-
-
-
-
-
-
-
-
+      		$('.js-recent-user').html($copy);
+    	}, function (error) {
+     	    console.error(err);
+      		window.alert('Something went wrong!');
+    	});
+  	});
 
 
 
@@ -64,3 +63,10 @@ $(document).ready(function() {
 
 
 });
+
+
+
+
+
+
+
